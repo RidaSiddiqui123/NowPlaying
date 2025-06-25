abf7d5641cf51b7d27717bef50e7540457e05e31
ridasiddiqui123 <ridasiddiqui123@gmail.com>
2025-06-24 18:54:36 -0700

    debugging

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
+  base: '/', // Ensures correct path resolution
+  publicDir: 'public', // This is the default, but good to be explicit
 })
